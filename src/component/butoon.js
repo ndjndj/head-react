48c9fd5ace9e8c02755988a2ae640ff66d65e8a4
ndjndj <teq.ndj@gmail.com>
2021-11-01 01:12:58 +0900

    skelton with button.js

---
+++ src/component/butoon.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Button extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <button>
+
+                </button>
+            </React.Fragment>
+        );
+    }
+}
+
+export default Button;
