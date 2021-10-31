393517a9448e67e0d6231c3d43d49a0bd87897b5
ndjndj <teq.ndj@gmail.com>
2021-10-31 20:39:10 +0900

    skelton

--- src/component/input-label.js
+++ src/component/input-label.js
@@ -1,1 +1,14 @@
 import React from 'react';
+
+class InputLabel extends React.Component {
+    render() {
+        return (
+            <React.Fragment>
+                <label></label>
+                <input />
+            </React.Fragment>
+        );
+    }
+}
+
+export default InputLabel;
